f1c63c420cc74a399f6c24c5ee5f15ec1106eea8
Vladimir <vladimir156xz@gmail.com>
2023-12-04 18:47:31 +0100

    some code in index and src/runner

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
-echo 'Hexlet one love.';
+require_once __DIR__ . '/vendor/autoload.php';
 
+use Hexlet\Php\Runner;
 
+print_r(Runner\run());
--- src/Runner.php
+++ src/Runner.php
@@ -4,7 +4,7 @@ namespace Hexlet\Php\Runner;
 
 function run()
 {
-	$collection = collect(['taylor', 'abigail', null])->map(function ($name) {
+    $collection = collect(['taylor', 'abigail', null])->map(function ($name) {
         return strtoupper($name);
     });
 
